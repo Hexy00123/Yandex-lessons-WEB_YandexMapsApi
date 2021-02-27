3188d7bca28de59d303a58c09857dd940c5c5ad4
hexy00123 <r.izmajlov2018@yandex.ru>
2021-02-27 21:08:29 +0400

    Большая задача по Maps API. Часть №1

--- Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API. Часть №1.py
@@ -1,30 +1,56 @@
 import os
 import sys
+import shutil
 import requests
 from PyQt5 import QtCore, QtWidgets, QtGui
 
 
-class Window(QtWidgets.QMainWindow):
+
+class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle('Example')
+        self.getImage()
+        self.initUI()
 
-        self.label = QtWidgets.QLabel('', self)
-        self.counter = 0
+    def initUI(self):
+        self.setGeometry(100, 100, 450, 450)
+        self.setWindowTitle('Отображение карты')
 
-    def keyPressEvent(self, event):
-        print(event.key())
+        self.pixmap = QtGui.QPixmap(self.map_file)
+        self.image = QtWidgets.QLabel(self)
+        self.image.resize(450, 450)
+        self.image.setPixmap(self.pixmap)
 
-        if event.key() == QtCore.Qt.Key_Q:
-            self.counter += 1
-            self.label.setText(f'Клавиша Q нажата {str(self.counter)} раз')
-            print(self.counter)
-        event.accept()
+    def getImage(self):
+        response = requests.request(method='GET',
+                                    url='https://static-maps.yandex.ru/1.x/',
+                                    params={
+                                        'll': '50.191374,53.217203',
+                                        'spn': '0.001500,0.001500',
+                                        'l': 'sat',
+                                        'size': '450,450'
+                                    })
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        try:
+            open('data/map.png')
+        except:
+            os.mkdir('data')
+
+        self.map_file = "data/map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def closeEvent(self, event):
+        shutil.rmtree('data')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    main_window = Window()
-    main_window.show()
-    sys.exit(app.exec_())
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec())
